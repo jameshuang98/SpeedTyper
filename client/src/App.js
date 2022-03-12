14cd4e51aeda30f8628091689416baed1efc667b
James <jameshuang337@gmail.com>
2022-03-11 17:25:30 -0800

    add random words

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,25 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
+import randomWords from 'random-words'
+
+const wordsDisplay = 200;
+const timeLimit = 60;
+
 
 function App() {
+
+  const [words, setWords] = useState([])
+
+  useEffect(() => {
+    setWords(generateWords());
+  }, [])
+
+  generateWords() {
+    return new Array(wordsDisplay).fill(null).map(() => randomWords())
+  }
+  
+
   return (
     <div className="App">
 
