f314ba3c485be70ebb97c72decee59ac7382a09a
jameshuang98 <jameshuang337@gmail.com>
2024-01-26 20:23:09 -0800

    create DTOs and DTO conversion methods

---
+++ server/DTOs/ScoreDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace server.DTOs
+{
+    public class ScoreDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CorrectWords { get; set; }
+        public int IncorrectWords { get; set; }
+        public int Characters { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ server/Extensions/DTOConversions.cs
@@ -0,0 +1,33 @@
+﻿using server.DTOs;
+using server.Entities;
+
+namespace server.Extensions
+{
+    public static class DTOConversions
+    {
+        public static UserDTO ConvertToDto(this User user)
+        {
+            return new UserDTO
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Username = user.Username,
+                ProfileImageURL = user.ProfileImageURL
+            };
+        }
+        public static ScoreDTO ConvertToDto(this Score score)
+        {
+            return new ScoreDTO
+            {
+                Id = score.Id,
+                UserId = score.UserId,
+                CorrectWords = score.CorrectWords,
+                IncorrectWords = score.IncorrectWords,
+                Characters = score.Characters,
+                CreatedDate = score.CreatedDate
+            };
+        }
+    }
+}
