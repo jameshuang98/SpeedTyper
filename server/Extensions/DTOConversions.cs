07f92c90c588dc046ab2484d85e18b863256b62f
jameshuang98 <jameshuang337@gmail.com>
2024-02-01 20:18:43 -0800

    Add new conversions

--- server/Extensions/DTOConversions.cs
+++ server/Extensions/DTOConversions.cs
@@ -1,33 +1,54 @@
 ﻿using server.DTOs;
 using server.Entities;
 
-namespace server.Extensions
+namespace server.Extensions;
+
+public static class DTOConversions
 {
-    public static class DTOConversions
+    public static UserDTO ConvertToDTO(this User user)
     {
-        public static UserDTO ConvertToDto(this User user)
+        return new UserDTO
         {
-            return new UserDTO
-            {
-                Id = user.Id,
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                Email = user.Email,
-                Username = user.Username,
-                ProfileImageURL = user.ProfileImageURL
-            };
-        }
-        public static ScoreDTO ConvertToDto(this Score score)
+            Id = user.Id,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            Email = user.Email,
+            Username = user.Username,
+            ProfileImageURL = user.ProfileImageURL
+        };
+    }
+    public static ScoreDTO ConvertToDTO(this Score score)
+    {
+        return new ScoreDTO
         {
-            return new ScoreDTO
-            {
-                Id = score.Id,
-                UserId = score.UserId,
-                CorrectWords = score.CorrectWords,
-                IncorrectWords = score.IncorrectWords,
-                Characters = score.Characters,
-                CreatedDate = score.CreatedDate
-            };
+            Id = score.Id,
+            UserId = score.UserId,
+            CorrectWords = score.CorrectWords,
+            IncorrectWords = score.IncorrectWords,
+            Characters = score.Characters,
+            CreatedDate = score.CreatedDate
+        };
+    }
+
+    public static IEnumerable<UserDTO> ConvertToDTO(this IEnumerable<User> users)
+    {
+        var userDTOs = new List<UserDTO>();
+        foreach (var user in users)
+        {
+            var userDTO = user.ConvertToDTO();
+            userDTOs.Add(userDTO);
         }
+        return userDTOs;
+    }
+
+    public static IEnumerable<ScoreDTO> ConvertToDTO(this IEnumerable<Score> scores)
+    {
+        var scoreDTOs = new List<ScoreDTO>();
+        foreach (var score in scores)
+        {
+            var scoreDTO = score.ConvertToDTO();
+            scoreDTOs.Add(scoreDTO);
+        }
+        return scoreDTOs;
     }
 }
