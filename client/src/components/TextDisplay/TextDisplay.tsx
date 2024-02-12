d4091fcc80b6cdb1bbec0867fb0bbd38e26e764f
jameshuang98 <jameshuang337@gmail.com>
2024-02-11 16:31:21 -0800

    refactor text display component

--- client/src/components/TextDisplay/TextDisplay.tsx
+++ client/src/components/TextDisplay/TextDisplay.tsx
@@ -23,24 +23,31 @@ function TextDisplay(props: Props) {
         }
     }, [gameState]);
 
+    let formattedWords = words.map((word, i) => (
+        <span key={i}>
+            <span className={getWordClass(i)} ref={wordRefs[i]}>
+                {word}
+            </span>
+            <span> </span>
+        </span>
+    ));
+
+    if (gameState === "playing") {
+        formattedWords = formattedWords.slice(lineIndexes[currLineIndex]);
+    }
+
     return (
         <div className="main-container">
             <div className="output-container">
-                <div className="text sample">
-                    {words.map((word, i) => (
-                        <span key={i}>
-                            <span className={getWordClass(i)} ref={wordRefs[i]}>
-                                {word}
-                            </span>
-                            <span> </span>
-                        </span>
-                    )).slice(lineIndexes[currLineIndex])}
+                <div className="text sample" style={gameState === "postgame" ? { overflowY: "scroll" } : {}}>
+                    {formattedWords}
                 </div>
             </div>
-
-            <div className="input-container">
-                <input disabled={gameState !== 'playing'} ref={textInput} type="text" onKeyDown={handleInput} value={input} />
-            </div>
+            {gameState !== "postgame" && (
+                <div className="input-container">
+                    <input disabled={gameState !== 'playing'} ref={textInput} type="text" onKeyDown={handleInput} value={input} />
+                </div>
+            )}
         </div>
     )
 }
