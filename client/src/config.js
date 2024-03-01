9f5bb9082a58b4c70a9912b61274e7bc505e2393
jameshuang98 <jameshuang337@gmail.com>
2024-03-01 15:20:11 -0800

    add axios and api urls

---
+++ client/src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    productionApiUrl: 'https://speedtyperapi.azurewebsites.net',
+    developmentApiUrl: 'https://localhost:7021'
+};
+
+export default config;
--- client/src/pages/ScoreHistory.tsx
+++ client/src/pages/ScoreHistory.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Typography } from '@mui/material'
+import axios from 'axios';
+import API_BASE_URL from "../constants/constants.js";
 
-type Props = {}
 
-export default function ScoreHistory({}: Props) {
-  return (
-    <div>ScoreHistory</div>
-  )
+type Props = {};
+
+export default function ScoreHistory() {
+    const [data, setData] = useState({email: ""});
+    console.log('process', process.env)
+    const getData = () => {
+        axios.get(`${API_BASE_URL}user/1`)
+            .then(response => {
+                console.log("response", response);
+                setData(response.data);
+            })
+            .catch(error => {
+                // Handle error
+            });
+    }
+
+    return (
+        <>
+            <h3>Scores</h3>
+            <Button variant="contained" size="large" onClick={getData}>
+                <Typography variant="subtitle2">Get Data</Typography>
+            </Button>
+
+            <Typography>{data.email ?? ""}</Typography>
+        </>
+    )
 }
