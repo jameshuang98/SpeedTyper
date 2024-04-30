6063a2a090dea9571ca8777d31a2efef8a65f975
jameshuang98 <jameshuang337@gmail.com>
2024-04-30 00:06:03 -0700

    appbar styling changes and add click handler to logo

--- client/src/components/Appbar/Appbar.tsx
+++ client/src/components/Appbar/Appbar.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import classes from './Appbar.module.scss';
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+import classes from './Appbar.module.scss';
 
 function Appbar() {
+    const navigate = useNavigate();
+
     return (
         <AppBar position="static" className={classes.appbar}>
-            <Toolbar>
-                <Typography variant="body1" component="div" className={classes.appName}>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="body1" component="div" className={classes.appName} onClick={() => navigate("/")}>
                     SpeedTyper
                 </Typography>
             </Toolbar>
