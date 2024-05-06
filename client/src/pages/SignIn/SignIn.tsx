ad9c719f0f6b8e8b22713c41c94600a9d5e723a3
jameshuang98 <jameshuang337@gmail.com>
2024-05-06 00:52:23 -0700

    implement signing in and signing up on clientside

--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -11,7 +11,10 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { UserLoginRequest } from 'constants/types';
+import { loginUser } from 'api/users';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'contexts/SnackbarContext';
 
 function Copyright(props: any) {
   return (
@@ -26,21 +29,29 @@ function Copyright(props: any) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-const defaultTheme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const navigate = useNavigate();
+  const { showSnackbar } = useSnackbar();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    const userLoginRequest: UserLoginRequest = {
+      email: data.get('email') as string,
+      password: data.get('password') as string
+    };
+
+    const loginResult = await loginUser(userLoginRequest);
+    console.log("loginResult", loginResult);
+    if (loginResult) {
+      showSnackbar("Login Successful");
+      navigate("/");
+    }
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <>
       <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
         <Grid
@@ -113,7 +124,7 @@ export default function SignIn() {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="register" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
@@ -123,6 +134,6 @@ export default function SignIn() {
           </Box>
         </Grid>
       </Grid>
-    </ThemeProvider>
+    </>
   );
 }
