beef404a231e8d44faf3acda9707417f77b620dc
jameshuang98 <jameshuang337@gmail.com>
2024-05-16 00:08:31 -0700

    add leaderboard api and ensure user is authorized when getting their own scores

--- server/Controllers/ScoreController.cs
+++ server/Controllers/ScoreController.cs
@@ -3,6 +3,7 @@ using server.Repositories;
 using server.Extensions;
 using server.Models.Entities;
 using Microsoft.AspNetCore.Authorization;
+using server.Services.Interfaces;
 
 namespace server.Controllers;
 
@@ -12,12 +13,15 @@ namespace server.Controllers;
 public class ScoreController : ControllerBase
 {
     private readonly IScoreRepository _scoreRepository;
+    private readonly IAuthService _authService;
 
-    public ScoreController(IScoreRepository scoreRepository)
+    public ScoreController(IScoreRepository scoreRepository, IAuthService authService)
     {
         _scoreRepository = scoreRepository;
+        _authService = authService;
     }
 
+    [Authorize(Roles = "Admin")]
     [HttpGet()]
     public async Task<ActionResult<IEnumerable<Score>>> GetScores()
     {
@@ -30,10 +34,28 @@ public class ScoreController : ControllerBase
         return Ok(scoreDTOs);
     }
 
-    [HttpGet("/GetUserScores/{id:int}")]
-    public async Task<ActionResult<IEnumerable<Score>>> GetScoresByUserId(int id)
+    [HttpGet("leaderboard")]
+    public async Task<ActionResult<IEnumerable<Score>>> GetScoreLeaderboard()
     {
-        var scores = await _scoreRepository.GetScoresByUserId(id);
+        var scores = await _scoreRepository.GetScores();
+        if (scores == null)
+        {
+            return NotFound();
+        }
+
+        var scoreDTOs = scores.OrderBy(s => s.CorrectWords).Take(10).ConvertToDTO();
+        return Ok(scoreDTOs);
+    }
+
+    [HttpGet("GetUserScores/{userId:int}")]
+    public async Task<ActionResult<IEnumerable<Score>>> GetScoresByUserId(int userId)
+    {
+        if (!_authService.IsUserAuthorized(userId))
+        {
+            return Forbid();
+        }
+
+        var scores = await _scoreRepository.GetScoresByUserId(userId);
         if (scores == null)
         {
             return NotFound();
@@ -42,6 +64,7 @@ public class ScoreController : ControllerBase
         return Ok(scoreDTOs);
     }
 
+    [Authorize(Roles = "Admin")]
     [HttpGet("{id:int}")]
     public async Task<ActionResult<Score>> GetScore(int id)
     {
