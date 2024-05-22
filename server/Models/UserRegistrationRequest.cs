ce7ccad7bc24d518984c1bf82185994a92cf1916
jameshuang98 <jameshuang337@gmail.com>
2024-05-22 07:20:32 -0700

    create validation for User Registration request

--- server/Models/UserRegistrationRequest.cs
+++ server/Models/UserRegistrationRequest.cs
@@ -1,10 +1,21 @@
-﻿namespace server.Models;
+﻿using server.Validators;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Models;
 
 public class UserRegistrationRequest
 {
+    [Required(ErrorMessage = "First name is required.")]
     public string FirstName { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Last name is required.")]
     public string LastName { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Username is required.")]
     public string Username { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
     public string Email { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [ComplexPassword(ErrorMessage = "Password must be complex enough.")]
     public string Password { get; set; } = string.Empty;
 }
---
+++ server/Validators/ComplexPasswordAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace server.Validators;
+
+public class ComplexPasswordAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var password = value as string;
+        if (string.IsNullOrEmpty(password))
+        {
+            return new ValidationResult("Password is required.");
+        }
+
+        var hasUpperCase = Regex.IsMatch(password, @"[A-Z]");
+        var hasLowerCase = Regex.IsMatch(password, @"[a-z]");
+        var hasDigits = Regex.IsMatch(password, @"[0-9]");
+        var hasSpecialChars = Regex.IsMatch(password, @"[\W_]");
+
+        if (!hasUpperCase || !hasLowerCase || !hasDigits || !hasSpecialChars)
+        {
+            return new ValidationResult("Password must be complex enough.");
+        }
+
+        return ValidationResult.Success;
+    }
+}
