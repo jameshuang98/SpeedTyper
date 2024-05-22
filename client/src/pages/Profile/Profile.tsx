b45b4d81c82b1fdee30852d3de68d2cce4010c8b
jameshuang98 <jameshuang337@gmail.com>
2024-05-22 08:10:45 -0700

    hide background image on profile page when screen size gets small

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -16,14 +16,16 @@ export default function Profile(props: Props) {
     if (userLoading) {
         return <Loading />
     }
-    
+
     if (!user && !userLoading) {
         return <div>You must be logged in!</div>
     }
 
     return (
         <div>
-            <img className={classes.image} alt="background sky" src="https://images.unsplash.com/photo-1587279535322-b20697908487?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <Hidden smDown>
+                <img className={classes.image} alt="background sky" src="https://images.unsplash.com/photo-1587279535322-b20697908487?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            </Hidden>
             <div className={classes.container}>
                 <Hidden mdDown>
                     <ProfileCard />
